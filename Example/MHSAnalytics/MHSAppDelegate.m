cc4cfd0814184b3f7b37af164783b17c156ccdf3
Visual <980495918@qq.com>
2022-07-08 10:26:49 +0800

    feat:MHSAnalytics

--- Example/MHSAnalytics/MHSAppDelegate.m
+++ Example/MHSAnalytics/MHSAppDelegate.m
@@ -7,12 +7,20 @@
 //
 
 #import "MHSAppDelegate.h"
-
+#import <MHSAnalytics.h>
 @implementation MHSAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    [MHSAnalytics startWithServerURL:@""];
+    //如果想控制上报触发的条数和时间间隔，请使用下面的API
+//    [MHSAnalytics startWithServerURL:@"" flushBulkSize:100 flushInterval:30];
+//    [[MHSAnalytics sharedInstance] openAnalytics:YES];//控制是否开启埋点，默认开启
+//    [[MHSAnalytics sharedInstance] privntLogs:YES];//控制台是否开启打印，默认开启
+//    [[MHSAnalytics sharedInstance] updateUserId:@""];//更新用户ID，根据业务需要
+//    [[MHSAnalytics sharedInstance] updateUserTypeWithType:MHSAnalyticsUserTypeUser];//更新用户角色，根据业务需要
+    [[MHSAnalytics sharedInstance] trackWithEvent:@""];//日志上报
+//    [[MHSAnalytics sharedInstance]  trackWithEvent:@"" content:@{} context:@{}];//带附加属性的上报
     return YES;
 }
 
